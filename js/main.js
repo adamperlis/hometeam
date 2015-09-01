36ffcdbc7399ec3cd41133d1b39b91cccb52d71b
Adam Perlis <adam.perlis@gmail.com>
2015-09-01 19:02:50 -0400

    added zoom capability and replaced images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $('.carousel').carousel({
 	interval: false
 });
+
+
+
+$('img').mousedown(function(e){
+	var e = window.event;
+
+    var posX = e.clientX;
+    var posY = e.clientY;
+
+    document.posx = posX;
+    document.posy = posY;
+
+	function zoom() {
+	var position = document.querySelectorAll("img");
+		position[0].style.transformOrigin = posX + 'px ' + posY + 'px';
+		position[1].style.transformOrigin = posX + 'px ' + posY + 'px';
+		position[2].style.transformOrigin = posX + 'px ' + posY + 'px';
+		position[3].style.transformOrigin = posX + 'px ' + posY + 'px';
+	}
+
+	zoom();
+
+})
+
+
