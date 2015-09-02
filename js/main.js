737865ff9573062ec154756d60751c9f9de253c4
Adam Perlis <adam.perlis@gmail.com>
2015-09-02 18:04:47 -0400

    got pan & zoom working a little better

--- js/main.js
+++ js/main.js
@@ -3,26 +3,83 @@ $('.carousel').carousel({
 });
 
 
+// //Pan & Zoom
+// var clickCount = 0;
 
-// $('img').mousedown(function(e){
-// 	var e = window.event;
+// 	//Zoom
+// 	$('img').click(function(e){
+// 		e.preventDefault();
 
-//     var posX = e.clientX;
-//     var posY = e.clientY;
+// 		var e = window.event;
 
-//     document.posx = posX;
-//     document.posy = posY;
+// 	    var posX = e.clientX;
+// 	    var posY = e.clientY;
 
-// 	function zoom() {
-// 	var position = document.querySelectorAll("img");
-// 		position[0].style.transformOrigin = posX + 'px ' + posY + 'px';
-// 		position[1].style.transformOrigin = posX + 'px ' + posY + 'px';
-// 		position[2].style.transformOrigin = posX + 'px ' + posY + 'px';
-// 		position[3].style.transformOrigin = posX + 'px ' + posY + 'px';
-// 	}
+// 	    document.posx = posX;
+// 	    document.posy = posY;
 
-// 	zoom();
+// 		if (clickCount == 0) {
+// 			function zoom() {
+// 			var position = document.querySelectorAll("img");
+// 				position[0].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 				position[1].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 				position[2].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 				position[3].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 				position[0].style.webkitTransform = "scale(2)";
+// 				position[1].style.webkitTransform = "scale(2)";
+// 				position[2].style.webkitTransform = "scale(2)";
+// 				position[3].style.webkitTransform = "scale(2)";
+// 				position[0].style.cursor = 'zoom-out';
+// 				position[1].style.cursor = 'zoom-out';
+// 				position[2].style.cursor = 'zoom-out';
+// 				position[3].style.cursor = 'zoom-out';
+// 			}
 
-// })
+// 			zoom();
+// 			clickCount = ++clickCount
 
+// 		} else {
+// 			function clearStyle(){
+// 				var position = document.querySelectorAll("img");
+// 				position[0].style.transformOrigin = '0px 0px';
+// 				position[1].style.transformOrigin = '0px 0px';
+// 				position[2].style.transformOrigin = '0px 0px';
+// 				position[3].style.transformOrigin = '0px 0px';
+// 				position[0].style.webkitTransform = "scale(1)";
+// 				position[1].style.webkitTransform = "scale(1)";
+// 				position[2].style.webkitTransform = "scale(1)";
+// 				position[3].style.webkitTransform = "scale(1)";
+// 				position[0].style.cursor = 'zoom-in';
+// 				position[1].style.cursor = 'zoom-in';
+// 				position[2].style.cursor = 'zoom-in';
+// 				position[3].style.cursor = 'zoom-in';
+// 			}
+// 			clearStyle();
+// 			clickCount = 0;
+// 		}
+
+// 	})
+
+// 	//Pan
+// 	$('img').mousemove(function(e){
+// 		e.preventDefault();
+
+// 		var e = window.event;
+
+// 	    var posX = e.clientX;
+// 	    var posY = e.clientY;
+
+// 	    document.posx = posX;
+// 	    document.posy = posY;
+
+// 	    function pan() {
+// 			var position = document.querySelectorAll("img");
+// 				position[0].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 				position[1].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 				position[2].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 				position[3].style.transformOrigin = posX + 'px ' + posY + 'px';
+// 			}
+// 			pan();
+
+// 	})
 
