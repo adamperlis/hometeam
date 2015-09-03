0988ca5f73f67bffb6fc50fc59326c41a7f168f8
Adam Perlis <adam.perlis@gmail.com>
2015-09-03 11:02:39 -0400

    fixed port number2

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var app = express();
 app.use('/', express.static(__dirname + '/'));
 app.listen(process.env.PORT || 5000)
 
-console.log('Running server at http://localhost:' + port + '/');
+console.log('We have lift off');
